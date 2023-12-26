a03ef34685a0c24015810a5f6cb7a51ea457c90d
IndraPoonam <iamindra.poonam@gmail.com>
2023-12-27 01:21:47 +0530

    about object

---
+++ node.js
@@ -0,0 +1,8 @@
+// let a=10;
+// let b=20;
+// let c=a+b;
+// console.log(c);
+let p=4; 
+let q=3;
+let z=p*q;
+console.log(z);
---
+++ object.js
@@ -0,0 +1,10 @@
+let lecture=10;
+let section=3;
+let title='javascript';
+
+const course={
+    lecture:10,
+    section:3,
+    title: 'javascript',
+    notes
+}
