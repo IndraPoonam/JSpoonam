45dcd422680f54f1fc66f2ff334ff554fbffe362
IndraPoonam <iamindra.poonam@gmail.com>
2023-12-29 11:14:06 +0530

    msh

---
+++ objectmethod.js
@@ -0,0 +1,30 @@
+var a={
+    name: 'poonam',
+    surname: 'singh',
+     place: 'rewa',
+     age: 24,
+     Number: 123654123,
+//      favHero: ['bachan','khan','deol'],
+// favcity:{
+// city: 'punjab',
+// country: 'india',
+// },
+//      salary: function(){
+//         return 30000;
+//      },
+//      fullname : function(){
+// return this.name +" "+ this.surname;
+//      }
+
+}
+// console.log(a);
+// console.log(Object.values(a));
+// console.log(Object.keys(a));
+// console.log(Object.assign(a));
+// console.log(Object.create(a));
+console.log(Object.getOwnPropertyNames(a));
+
+// document.write(a.favHero[2]);
+// document.write(a.salary());
+// document.write(a.fullname());
+// document.write(a.favcity.city);
